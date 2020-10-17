dfbb71da1609777e2910a187ddb06dbebbdb6957
Alexander Yanin <alexandr.nn54@gmail.com>
2020-10-17 19:05:52 +0700

    Add a solution to the problem 263/B, fix Template

--- src/main/java/codeforces/archieve/c263/B.java
+++ src/main/java/codeforces/archieve/c263/B.java
@@ -1,11 +1,35 @@
-package codeforces;
+package codeforces.archieve.c263;
 
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Template {
+public class B {
     public static void solve() {
+        int[] nk = Scan.intArray();
+        int[] a = IntStream.of(Scan.intArray()).boxed().sorted(Collections.reverseOrder()).mapToInt(i -> i).toArray();
+        int n = nk[0];
+        int k = nk[1];
+        int x = a[0];
+        int countIncluding = 0;
+        String result = "-1";
 
+        if (n < k) {
+            System.out.println(result);
+            return;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (countIncluding == k - 1) {
+                x = a[i];
+                break;
+            }
+
+            countIncluding++;
+        }
+
+        System.out.println(x + " " + 0);
     }
 
     public static void main(String[] args) {
@@ -32,7 +56,7 @@ public class Template {
             int[][] matrix = new int[size][size];
 
             for (int i = 0; i < size; i++) {
-                int[] row = Stream.of(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                int[] row = Stream.of(scanner.next().split(" ")).mapToInt(Integer::parseInt).toArray();
                 matrix[i] = row;
             }
 
