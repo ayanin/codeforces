19ff36c62737ecc8287741ae863ef9ea2f616483
Alexander Yanin <alexandr.nn54@gmail.com>
2020-10-20 17:38:27 +0700

    Update template

--- src/main/java/codeforces/Template.java
+++ src/main/java/codeforces/Template.java
@@ -1,7 +1,12 @@
 package codeforces;
 
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Template {
     public static void solve() {
@@ -10,37 +15,130 @@ public class Template {
 
     public static void main(String[] args) {
         solve();
-        Scan.scanner.close();
+        FS.close();
     }
 
-    static class Scan {
-        static private Scanner scanner = new Scanner(System.in);
+    static class FS {
+        private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private static StringTokenizer st = new StringTokenizer("");
+        static PrintWriter pt = new PrintWriter(System.out);
 
-        public static String nextLine() {
-            return scanner.nextLine();
+        static String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
         }
 
-        public static int nextInt() {
-            return scanner.nextInt();
+        static int nextInt() {
+            return Integer.parseInt(next());
         }
 
-        public static long nextLong() {
-            return scanner.nextLong();
+        static int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
         }
 
-        public static int[][] numberMatrix(int size) {
-            int[][] matrix = new int[size][size];
-
-            for (int i = 0; i < size; i++) {
-                int[] row = Stream.of(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                matrix[i] = row;
+        static int[][] read2Array(int m, int n) {
+            int[][] a = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
             }
-
-            return matrix;
+            return a;
         }
 
-        public static int[] intArray() {
-            return Stream.of(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        static long[][] read2ArrayL(int m, int n) {
+            long[][] a = new long[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        void printArr(long[] arr) {
+            for (long value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static long[] readArrayL(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        static void printArr(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void printArrL(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void print2Arr(int[][] arr, int m, int n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static void print2Arr(long[][] arr, int m, int n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static List<Integer> readListInt(int n) {
+            List<Integer> list = new ArrayList<Integer>(n);
+            for (int i = 0; i < n; i++) {
+                list.add(nextInt());
+            }
+            return list;
+        }
+
+        static <T> void printList(List<T> list) {
+            for (T value : list) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void close() {
+            pt.close();
+        }
+
+        static long nextLong() {
+            return Long.parseLong(next());
         }
     }
 }
